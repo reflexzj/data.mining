8bed83ba4bdd26fb3c1f030905025e5f2dd8fded
reflex_zj <813577079@qq.com>
2017-06-02 15:35:40 +0800

    Initial commit

---
+++ 2017-6-2-sesion2/do.py
@@ -0,0 +1,71 @@
+# coding=utf-8
+from gensim import corpora
+
+# 产生对应格式的texts
+def to_texts(file):
+    datas = open(file, 'r')
+    texts = [[word for word in data.split(',')]
+             for data in datas]
+    return texts
+
+# 统计维度，并生成对应的字典
+# 搜集停用词，仅出现一次词的id
+def tokens(texts):
+    dictionary = corpora.Dictionary(texts)
+    dictionary.save('temp/tokens.dict')
+
+    #停用词
+    # stop_ids = [dictionary.token2id[stopword] for stopword in stoplist
+    #             if stopword in dictionary.token2id]、
+
+    # 出现一次的低频词
+    once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems()
+                if docfreq == 1]
+
+    #删除停用词和低频词，并去除不连续的缺口
+    dictionary.filter_tokens(once_ids)
+    dictionary.compactify()
+
+    # for index,data in dictionary.token2id.items():
+    #     print index,data
+
+    return dictionary
+
+#产生稀疏文档向量
+def to_vectors(dictionary, texts):
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    #存入硬盘备用
+    corpora.MmCorpus.serialize('temp/vectors.mm', corpus)
+
+    # for data in corpus:
+    #     print data
+
+    #将处理好一个稀疏矩阵存入一个text文件中，有没有这个必要？
+    # fopen = open('result/corpus.txt','w')
+    # for data in corpus:
+    #     for word in data:
+    #         fopen.write(word)
+    #         fopen.write(',')
+    #     fopen.write('\n')
+
+    return corpus
+
+# 不同的语料库格式
+def corpus_formate(corpus):
+    #存储为不同的语料
+    corpora.SvmLightCorpus.serialize('data/corpus.svmlight', corpus)
+    corpora.BleiCorpus.serialize('data/corpus.lda-c', corpus)
+    corpora.LowCorpus.serialize('data/corpus.low', corpus)
+
+    #载入语料库
+    corpus = corpora.MmCorpus('temp/vectors.mm')
+
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    file = 'data/result.txt'
+    texts= to_texts(file)
+    dictionary = tokens(texts)
+    vectors = to_vectors(dictionary, texts)
