014d0672a32bd8faa16299f21d5a5a9ced02703d
reflex_zj <813577079@qq.com>
2017-06-20 15:05:49 +0800

    lda_understand

--- 2017-6-5-sesion3/do_lda.py
+++ 2017-6-5-sesion3/do_lda.py
@@ -1,11 +1,14 @@
 from gensim import corpora, models
-import do_tfidf
 
 def to_ldaModel(corpus):
+
     lda = models.LdaModel(corpus, num_topics= 9)
+
     fopen = open('data/lda_file.csv', 'w')
     for each in lda[corpus]:
         fopen.write(str(each)+ '\n')
+
+
 
 if __name__ == '__main__':
     corpus_url = '../2017-6-2-sesion2/temp/vectors.mm'
