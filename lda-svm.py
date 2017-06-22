5bd8ecccbc8abde9fd8d34c7cab2eea374e928ce
reflex_zj <813577079@qq.com>
2017-06-22 15:52:30 +0800

    Adjustment parameters

--- lda-svm.py
+++ lda-svm.py
@@ -8,24 +8,22 @@ def lda_svm(topic_num, rate):
         对于全新的文本库，需要进行预处理操作
         执行data_processing, 将所有的文本读入一个result.txt文件中去
         执行build_corpus, 生成对应的维度词典以及语料库
-
         LDA生成主题模型，投入对应的SVM模型中去
         期间所有的中间文件，存储在data_mining_learning/data下
     '''
 
     # 给出LDA主题模型，自己定义LDA的主题数目
     corpus_url = 'build_corpus/temp/vectors.mm'
-
-
     corpus = corpora.MmCorpus(corpus_url)
+
     do_lda.to_ldaModel(corpus, topic_num)
 
     # 投入svm模型中去（LDA的主题分布函数，则为SVM模型的属性）
     x_file = 'data/train_x.csv'
     y_file = 'data/train_y.csv'
 
-
     do_svm.do_svm(x_file, y_file, rate)
+
 
 if __name__ == '__main__':
 
--- svm_model/do_svm.py
+++ svm_model/do_svm.py
@@ -1,6 +1,6 @@
 # coding=utf-8
-from sklearn import svm
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn import svm, preprocessing
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
 import numpy as np
 import pandas as pd
 import time
@@ -11,20 +11,28 @@ def do_svm(x_file, y_file, rate):
     x = pd.read_csv(x_file, header=None)
     y = pd.read_csv(y_file, header=None)
 
-    # 传统的验证集，测试集的划分
-    # train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=rate, random_state= 11)
-    #
-    # svc_model = svm.SVC(kernel= 'linear', C=1)
+    # 样本的标准化处理（这里，标签Y并不需要标准化处理）
+    scaler = preprocessing.StandardScaler().fit(x)
+    x_transform = scaler.transform(x)
+    print type(x_transform), type(x)
+
+    svc_model = svm.SVC(kernel= 'linear', C=1)
+
+    # （1）传统的验证集，测试集的划分
+    # train_x, test_x, train_y, test_y = train_test_split(x_transform, y, test_size=rate, random_state= 11)
+
     # svc_model.fit(train_x, train_y)
     # result = svc_model.score(test_x, test_y)
 
-    # 交叉验证法（cross_val_score）
+    # （2）交叉验证法（cross_val_score）,用法好像有点问题
+    y = y.values[: , 0]
     svc_model = svm.SVC(kernel= 'linear', C=1)
-    result = cross_val_score(svc_model, x, y, cv=2)
+    result = cross_val_score(svc_model, x_transform, y, cv=5)
+
 
     end = time.time()
 
-    print '----------svm model leaning and show the result:', start-end, '\n', result
+    print '---------- svm model leaning and show the result, time cost:', start-end, '\n', result
 
 
 
