0cb29ca40659486d8c37e79553c82c6fdc84c002
reflexzj <813577079@qq.com>
2017-06-22 12:54:03 +0800

    update

--- lda-svm.py
+++ lda-svm.py
@@ -3,7 +3,7 @@ from tfidf_LDA import do_lda
 from svm_model import do_svm
 from gensim import corpora
 
-def lda_svm():
+def lda_svm(topic_num, rate):
     '''
         对于全新的文本库，需要进行预处理操作
         执行data_processing, 将所有的文本读入一个result.txt文件中去
@@ -16,7 +16,6 @@ def lda_svm():
     # 给出LDA主题模型，自己定义LDA的主题数目
     corpus_url = 'build_corpus/temp/vectors.mm'
 
-    topic_num = 40
 
     corpus = corpora.MmCorpus(corpus_url)
     do_lda.to_ldaModel(corpus, topic_num)
@@ -25,8 +24,13 @@ def lda_svm():
     x_file = 'data/train_x.csv'
     y_file = 'data/train_y.csv'
 
-    rate = 1 / 4.0
+
     do_svm.do_svm(x_file, y_file, rate)
 
 if __name__ == '__main__':
-    lda_svm()
+
+    # 给定参数
+    topic_num = 50
+    test_rate = 1/4.0
+
+    lda_svm(topic_num, test_rate)
