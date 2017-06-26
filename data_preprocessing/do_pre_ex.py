948be5b62e4166fab7735e132028017ab74c87ee
reflex_zj <813577079@qq.com>
2017-06-26 11:45:52 +0800

    introduction

--- data_preprocessing/do_pre_ex.py
+++ data_preprocessing/do_pre_ex.py
@@ -1,5 +1,8 @@
+# coding=utf-8
 import os
 
+# os.walk方法以最小的子文件夹为单位，读取该文件夹下所有的内容
+# 文件路径，好像不区分正反斜杠（默认反斜杠）
 rootdir = '../data_01/'
 for parent, dirnames, filenames in os.walk(rootdir):
     for filename in filenames:
