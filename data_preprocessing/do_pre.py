d0afea1ba98003ddb5441a5f000e21eac5e44598
reflex_zj <813577079@qq.com>
2017-06-26 11:31:48 +0800

    introduction

--- data_preprocessing/do_pre.py
+++ data_preprocessing/do_pre.py
@@ -24,33 +24,38 @@ def handle(filename):
     f = open(url_text, 'r')
     txt_string = f.read().strip().decode('utf-8').replace('\n', ' ')
 
-    # 获取中文内容
+    # 获取中文内容，移除停用词，移除重复词语（一般为了词频，并不执行这个操作）
     step_1 = sentence.filter_chinese_character(txt_string)
-    # 移除停用词
     step_2 = sentence.remove_stop_word(step_1)
-    # 移除重复词
     # step_3 = sentence.remove_repeat_word(step_2)
 
     return step_2
 
-if __name__ == '__main__':
+def get_result(filepach):
 
-    # 获取九个文件夹的目录
+    # 获取文件夹的根目录, 将数据文本的rar文件解压到制定目录下（获得了分类好的子文件夹）
     datas = []
-    # 将rar文件解压到当前目录
-    filepach = 'data/data/'
     datas = eachFile(filepach)
 
     # 获取每个文件夹下的文件目录
     fopen = open('data/result.txt', 'w')
     for each in datas:
-        filepach = each+'/'
+        filepach = each + '/'
         files = eachFile(filepach)
 
         # 遍历所有的每个文件夹下的所有txt文件
         for filename in files:
             data = handle(filename)
-            print data
+            # print data
             if data:
-                fopen.write(data+'\n')
+                fopen.write(data + '\n')
             print filename + 'has done'
+
+if __name__ == '__main__':
+
+    rootpach = 'data/data/'
+    get_result(rootpach)
+
+
+
+
