12a8d8adfa3ec232e325c33c408ee5a63d1800e0
reflex_zj <813577079@qq.com>
2017-07-14 16:34:22 +0800

    introduction

--- svm_model/do_svm.py
+++ svm_model/do_svm.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from sklearn import svm, preprocessing
+from sklearn import svm, preprocessing, metrics
 from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
 import numpy as np
 import pandas as pd
@@ -13,15 +13,19 @@ def do_svm(x_file, y_file, rate, cv):
 
     # 样本的标准化处理（这里，标签Y并不需要标准化处理）
     scaler = preprocessing.StandardScaler().fit(x)
+    print scaler
     x_transform = scaler.transform(x)
 
     svc_model = svm.SVC(kernel= 'linear', C=1)
 
     # （1）传统的验证集，测试集的划分
     # train_x, test_x, train_y, test_y = train_test_split(x_transform, y, test_size=rate, random_state= 11)
-
+    #
     # svc_model.fit(train_x, train_y)
     # result = svc_model.score(test_x, test_y)
+    #
+    # prediction = svc_model.predict(test_x)
+    # result = metrics.classification_report(test_y, prediction)
 
     # （2）交叉验证法（cross_val_score）,用法好像有点问题
     y = y.values[: , 0]
